59b85c1959b9b03b149317e96bcc665cb51de1f6
Frank <frank.uneweb@gmail.com>
2023-02-17 17:10:44 -0400

    finish

--- exercices/exercice_25.py
+++ exercices/exercice_25.py
@@ -1,2 +1,10 @@
+def fib2(n=10): # devuelve la serie de Fibonacci hasta n
+   """Devuelve una lista conteniendo la serie de Fibonacci"""
+   result = []
+   a, b = 0, 1
+   while a < n:
+      result.append(a)
+      a, b = b, a+b
+   print(result)
 
-    
+fib2(100)
